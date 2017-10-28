02003af008201ff9a62d1efc0a5988065ab258d8
dimsol <claudiosakaeshigemi@gmail.com>
2017-10-28 11:15:48 -0200

    atualizando os métodos on resume

--- ButtonClickCounter/app/src/main/java/com/example/claud/buttonclickcounter/MainActivity.java
+++ ButtonClickCounter/app/src/main/java/com/example/claud/buttonclickcounter/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.claud.buttonclickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,17 +11,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TEXT_CONTENTS = " TextContents";
     private EditText userInput;
-    private Button button;
     private TextView textView;
-    private int numTimesClicked = 0;
+    private static final String TAG = "MainActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         userInput = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
+        userInput.setText("");
+        Button button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         textView.setText("");
         textView.setMovementMethod(new ScrollingMovementMethod() );
@@ -29,16 +34,97 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener orOwnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numTimesClicked = numTimesClicked + 1;
-                String result = " The button got tapped " + numTimesClicked + "time";
-                if ( numTimesClicked !=1 ){
-                    result += "s"; // tem o mesmo resultado de result = result +"s"
-                }
-                result += "\n";
-
+                String result = userInput.getText().toString();
+                result = result + "\n";
                 textView.append(result);
+                userInput.setText("");
             }
         };
         button.setOnClickListener(orOwnClickListener);
+        Log.d(TAG, "oncreate: out. ");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState :  in");
+        super.onRestoreInstanceState(savedInstanceState);
+//        String savedString = savedInstanceState.getString(TEXT_CONTENTS);
+//        textView.setText(savedString);
+        //versão resumida
+        textView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState : out ");
+    }
+
+    @Override
+    protected void onRestart() {
+
+        Log.d(TAG, " onRestart: in ");
+        super.onRestart();
+        Log.d(TAG,"onRestart : out " );
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, " onSaveInstanceState: in ");
+        outState.putString( TEXT_CONTENTS, textView.getText().toString() );
+        super.onSaveInstanceState(outState);
+        Log.d(TAG,"onSaveInstanceStateonSaveInstanceState : out " );
+    }
+
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, " onPause: in ");
+        super.onPause();
+        Log.d(TAG,"onPause : out " );
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, " onResume: in ");
+        super.onResume();
+        Log.d(TAG," onResume : out " );
+    }
+
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, " onStop: in ");
+        super.onStop();
+        Log.d(TAG,"onStop : out " );
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
